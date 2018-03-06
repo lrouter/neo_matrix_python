7b353784ae0aaac22d557423f401b3b9a9530b12
lrouter <kaixi.f@gmail.com>
2018-03-06 11:06:49 +0800

    Update matrix_board_init.py
    rewrite gpio mapping data structure;
    refine gpio export and unexport code;

--- matrix_board_init.py
+++ matrix_board_init.py
@@ -11,25 +11,25 @@ import time
 import matrix_logger as mlog
 
 class BoardInit(object):
-		
-		#GPIOs map
-		#GPIOA - 0, GPIOB - 32
-		__gpio_map = {'led_upload': '203',
-					'led_usrcfg_0': '66',
-					'led_usrcfg_1': '67',
-					'button_phtoto': '64',
-					'button_restore': '65',
-					'gpio_a3': '3',
-					'gpio_a6': '6',
-					'gpio_a17': '17',
-					'gpio_g8': '200',
-					'gpio_g9': '201'
-					};
-		#GPIO dir			
-        __gpio_dir = '/sys/class/gpio'
-        __gpio_export_dir = '/sys/class/gpio/export'
-		__gpio_unexport_dir = '/sys/class/gpio/unexport'
-		
+        
+    #GPIOs map
+    #GPIOA - 0, GPIOB - 32
+    __gpio_map = {'led_upload': '203',
+                'led_usrcfg_0': '66',
+                'led_usrcfg_1': '67',
+                'button_phtoto': '64',
+                'button_restore': '65',
+                'gpio_a3': '3',
+                'gpio_a6': '6',
+                'gpio_a17': '17',
+                'gpio_g8': '200',
+                'gpio_g9': '201'
+                };
+    #GPIO dir           
+    __gpio_dir = '/sys/class/gpio'
+    __gpio_export_dir = '/sys/class/gpio/export'
+    __gpio_unexport_dir = '/sys/class/gpio/unexport'
+        
     ######################################################
     #   Method - __init__
     ######################################################    
@@ -38,23 +38,23 @@ class BoardInit(object):
         self.__logger = mlog.MatrixLogger("BoardInit")
         
     ##############################################################
-    # 				Board init function	
+    #               Board init function 
     ##############################################################
     def board_init(self):
         ##
         ##creat gpio inode in sysfs filesystem
         ##
 
-		#export GPIO pin
+        #export GPIO pin
         cmdstr = 'chmod 700 ' + self.__gpio_export_dir
         os.system(cmdstr)
-		for gpio_name, gpio_number in gpio_map.items():
-			gpio_pin_dir = os.path.join(self.__gpio_dir, 'gpio' + gpio_number)
-			cmdstr = 'echo ' + gpio_number + ' > ' + self.__gpio_export_dir
-			if os.path.exists(gpio_pin_dir):
-				pass
-			else:
-				os.system(cmdstr)
+        for gpio_name, gpio_number in gpio_map.items():
+            gpio_pin_dir = os.path.join(self.__gpio_dir, 'gpio' + gpio_number)
+            cmdstr = 'echo ' + gpio_number + ' > ' + self.__gpio_export_dir
+            if os.path.exists(gpio_pin_dir):
+                pass
+            else:
+                os.system(cmdstr)
 
         ##
         ##creat adxl34x inode in sysfs filesystem
@@ -68,7 +68,7 @@ class BoardInit(object):
 
 
     ##############################################################
-    # 				Board uninit function	
+    #               Board uninit function   
     ##############################################################
     def boad_uninit(self):
 
@@ -77,11 +77,11 @@ class BoardInit(object):
         # delet releated file inode in sysfs filesystem
         cmdstr = 'chmod 700 ' + self.__gpio_unexport_dir
         os.system(cmdstr)
-		for gpio_name, gpio_number in gpio_map.items():
-			gpio_pin_dir = os.path.join(self.__gpio_dir, 'gpio' + gpio_number)
-			cmdstr = 'echo ' + gpio_number + ' > ' + self.__gpio_unexport_dir
-			if os.path.exists(gpio_pin_dir):
-				os.system(cmdstr)
+        for gpio_name, gpio_number in gpio_map.items():
+            gpio_pin_dir = os.path.join(self.__gpio_dir, 'gpio' + gpio_number)
+            cmdstr = 'echo ' + gpio_number + ' > ' + self.__gpio_unexport_dir
+            if os.path.exists(gpio_pin_dir):
+                os.system(cmdstr)
 
         #
         # delet adxl34x inode in sysfs filesystem
